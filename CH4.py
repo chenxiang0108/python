57f69ce3ea4039502c5601d4d4ee2d0b38084c6e
陳志祥 <48426608+chenxiang0108@users.noreply.github.com>
2019-03-18 09:23:42 +0800

    Delete CH4.py

--- CH4.py
+++
@@ -1,6 +0,0 @@
-def myage(age,height):
-    print('Im %s years old and %s height'%(age,height))
-myage(20,160)
-def thebankaccount(the_cash,bank_saving):
-    return the_cash + bank_saving
-print(thebankaccount(10,20))
